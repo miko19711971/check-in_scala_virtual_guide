c19ef474ef5fa02734b6de4345291a588c3f1630
miko19711971 <mikbondi@gmail.com>
2025-08-14 15:46:28 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,17 +1,12 @@
 {
-  "name": "check-in-scala-virtual-guide",
+  "name": "check-in_scala_virtual_guide",
   "version": "1.0.0",
-  "private": true,
   "type": "module",
-  "main": "index.js",
   "scripts": {
     "start": "node index.js"
   },
   "dependencies": {
-    "express": "^4.19.2",
-    "cors": "^2.8.5"
-  },
-  "engines": {
-    "node": ">=18 <23"
+    "cors": "2.8.5",
+    "express": "4.19.2"
   }
 }
